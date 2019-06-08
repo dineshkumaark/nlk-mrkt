bd560da7a26b728307c341f35c4a8b1605b3c38f
Dinesh-Kumaar <dineshkamara@gmail.com>
2019-06-08 18:19:54 +0530

    Branding Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/navbar/NavBar';
+import './components/navbar/Navbar.css';
+import {Sidebar } from './components/navbar/Sidebar';
 function App() {
   return (
     <div className="App">
         <NavBar brand="NLK" highlight="MRKT"/>
+        <Sidebar />
     </div>
   );
 }
