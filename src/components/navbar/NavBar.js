46ef04475c231505a1d3425a0526c080ff1f4021
Dinesh-Kumaar <dineshkamara@gmail.com>
2019-06-16 20:14:41 +0530

    Shadows Applied

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -4,7 +4,7 @@ function NavBar(props){
         <div className="container">
         <nav>
             <div className="brand">
-            <h4><img src="./Images/brand.png" height="50px"/></h4>
+            <h4><img src="./Images/brand.png" height="50px" alt="brand"/></h4>
             <span className="solidBrand">{props.brand}<span className="mark">{props.highlight}</span></span>
             </div>
             <div className="main-nav">
@@ -18,7 +18,7 @@ function NavBar(props){
                         <li><a href="#">My Account</a></li>
                         <li><a href="#">Orders</a></li>
                         <li><a href="#">Coupouns</a></li>
-                        <li><img src="./Images/shopping-basket.svg" width="30px" height="30px"/></li>
+                        <li><img src="./Images/shopping-basket.svg" width="30px" height="30px" alt="shopping basket"/></li>
                     </ul>
                 </div>
             </div>
