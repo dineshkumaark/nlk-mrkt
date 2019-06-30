044b5b7fdbbbf6e0267bda2963609336d8c815df
Dinesh-Kumaar <dineshkamara@gmail.com>
2019-06-30 20:34:07 +0530

    Responsive going

--- src/components/products/CurrentProduct.js
+++ src/components/products/CurrentProduct.js
@@ -18,40 +18,40 @@ class CurrentProduct extends Component{
         return(
             <div className="currentproduct">
                 <div className="p-name">
-                    <h5>{name}</h5>
-                    <div className="p-weight">
-                        <p>{weight}KG</p>
-                        <p className="p-price">{formatter.format(price)}</p>
-                    </div>
+                <h5>{name}</h5>
+                <div className="p-weight">
+                    <p>{weight}KG</p>
+                    <p className="p-price">{formatter.format(price)}</p>
                 </div>
-                <div className="p-image">
-                    <img src={img} alt={name} width="140px" height="100px"/>
-                </div>
-                <div className="p-desc">
-                  <div className="w-desc">
-                   <div>
-                    <label>Kg</label>
-                    <select>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                    </select>
-                   </div>
-                     <p className="p-or">or</p>
-                     <div>
-                     <label style={{marginTop:'13%'}}>Piece</label>
-                    <select>
-                        <option></option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                    </select>
-                     </div>
-                  </div>
-                  <div className="p-basket">
-                        <button className="p-button">Add to Basket</button>
-                  </div>
-                </div>
+            </div>
+            <div className="p-image">
+                <img src={img} alt={name} width="140px" height="100px"/>
+            </div>
+            <div className="p-desc">
+              <div className="w-desc">
+               <div>
+                <label>Kg</label>
+                <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
+               </div>
+                 <p className="p-or">or</p>
+                 <div>
+                 <label style={{marginTop:'13%'}}>Piece</label>
+                <select>
+                    <option></option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
+                 </div>
+              </div>
+              <div className="p-basket">
+                    <button className="p-button">Add to Basket</button>
+              </div>
+            </div>
             </div>
         );
     }
