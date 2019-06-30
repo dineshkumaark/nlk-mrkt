d9ec94b7b31c8b09c5e8ce386d599ab289f3cd8b
Dinesh-Kumaar <dineshkamara@gmail.com>
2019-06-30 18:38:17 +0530

    Responsive going

--- src/components/navbar/ProductNavbar.js
+++ src/components/navbar/ProductNavbar.js
@@ -2,6 +2,10 @@ import React,{Component} from 'react';
 import slide from './slide';
 
 class ProductNavbar extends Component{ 
+    state = {
+        categories: ['Groceries','Steak,fish','Dairy','Food','Beverage','Cleaning'],
+        imgs: ['c-groceries.png','c-turkey.png','c-apple.png','c-diet.png','c-salad.png','c-asparagus.png']
+    }
     componentWillMount(){
         console.log("hi everyone");
     }
@@ -23,20 +27,28 @@ class ProductNavbar extends Component{
         console.log('updating');
     }
     render(){
+        const {categories,imgs} = this.state;
         return(
             <div className="container">
                 <div className="c-head">
                   <h1>Categories</h1>
                 </div>
                 <div className="categories">
-                    <ul className="c-ul">
-                        <li className="c-active p-li">Groceries</li>
-                        <li className="p-li">Steak,fish</li>
-                        <li className="p-li">Dairy</li>
-                        <li className="p-li">Food</li>
-                        <li className="p-li">Beverage</li>
-                        <li className="p-li">Cleaning</li>
-                    </ul>
+                <ul className="c-ul">
+                
+                {categories.map((item,index)=>(
+                        (index === 0) ?<li className="c-active p-li" key={index}>{item}</li> :<li className="p-li" key={index}>{item}</li>
+                ))}
+                </ul>
+                    <div className="c-hide">
+                        {categories.map((item,index)=>(
+                            <div className="f-prod" key={index}>
+                                <img src={`./Images/Veggie/${imgs[index]}`} width="35px" height="35px" alt={item}/>
+                                <p>{item}</p>
+                            </div>
+                        ))}
+                    </div>
+                    
                 </div>
             </div>
         );
