d8462c4b788228fce9fd827d142ec786622b9cf0
Dinesh-Kumaar <dineshkamara@gmail.com>
2019-07-16 22:21:52 +0530

    Almost Completed Cart

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,11 +3,12 @@ import {connect} from 'react-redux';
 // import  addCounter  from '../../actionTypes/action';
 function NavBar(props){
     const showCart= ()=>{
-        const { cart ,  count } = props;
-        props.showModel();
-        if(cart.length === 0){
+        const { carts ,  count } = props;
+         props.showModel();
+        if(carts.length === 0){
+            // alert('No');
         }else{
-            console.log(cart);
+            console.log(carts);
             console.log(count);
         }
     }
@@ -29,11 +30,27 @@ function NavBar(props){
                         <li><a href="#" className="m-hide">My Account</a></li>
                         <li><a href="#" className="m-hide">Orders</a></li>
                         <li><a href="#" className="m-hide">Coupouns</a></li>
-                        <li onClick={()=> showCart()}><img src="./Images/shopping-basket.svg" width="30px" height="30px" alt="shopping basket"/>{(props.cart.length > 0) ? <span className="badge">{props.cart.length}</span> : ''}</li>
+                        <li onClick={()=> showCart()}><img src="./Images/shopping-basket.svg" width="30px" height="30px" alt="shopping basket"/>{(props.carts.length > 0) ? <span className="badge">{props.carts.length}</span> : ''}</li>
                     </ul>
                     {(props.cart.showModel) ?
                      <div className="modal">
-                        <img src="./Images/Veggie/empty-cart.png"/>
+                        {(props.carts.length > 0) ? 
+                            <React.Fragment>
+                                {props.carts.map((item)=>(
+                                    <div>
+                                        <h1>{(item.name).toUpperCase()}</h1>
+                                        <p>{item.price}</p>
+                                    </div>
+                                )
+                                )}
+                            </React.Fragment>
+                        :
+                            <React.Fragment>
+                                <img src="./Images/Veggie/empty-cart.png"/>
+                                <h5>Your Cart is Empty!</h5>
+                            </React.Fragment>
+                        }
+                        
                      </div>
                      :''}
                 </div>
@@ -44,6 +61,7 @@ function NavBar(props){
 }
 const mapStatetoProps = (state) =>{
     return{
+        carts: state.current.cart,
         cart: state.cart,
         count: state.current.count
     }
